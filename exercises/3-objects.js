47ff52b36ac3e0ac68091f823163407d59c1a41a
Kris <109919530+maleker86@users.noreply.github.com>
2024-03-12 19:51:12 -0700

    Add an animal factory & destruct an array

--- exercises/3-objects.js
+++ exercises/3-objects.js
@@ -1,24 +1,42 @@
-var cat = {
-    legs: 3,
-    name: "Harmony",
-    color: "black",
-    speak: speak,
-    sound: "meow",
-    species: "cat",
-};
+function Animal(name, species, sound) {
+      return {
+        name: name,
+      species: species,
+      sound: sound,
+      speak: function () {
+        console.log(
+          this.sound + "! " + "I am a " + this.species + " named " + this.name,
+        )
+      }
+    };
+  }
 
-var pig = {
-    sound: "oink",
-    name: "Charlie",
-    speak: speak,
-}
+  const array = [1,2,3,4,5];
+  const [ zeroth, firsthe ] = array;
 
-var horse = {
-    sound: "neigh",
-    name: "Mary",
-    speak: speak,
-}
-
-function speak() {
-    console.log(this.sound + "! My name is " + this.name + "!")
-};
+  
+  // var cat = {
+  //     legs: 3,
+  //     name: "Harmony",
+  //     color: "black",
+  //     speak: speak,
+  //     sound: "meow",
+  //     species: "cat",
+  // };
+  
+  // var pig = {
+  //     sound: "oink",
+  //     name: "Charlie",
+  //     speak: speak,
+  // }
+  
+  // var horse = {
+  //     sound: "neigh",
+  //     name: "Mary",
+  //     speak: speak,
+  // }
+  
+  // function speak() {
+  //     console.log(this.sound + "! My name is " + this.name + "!")
+  // };
+  
