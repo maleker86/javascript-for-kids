8a41678c570af3b79ffd1710afe4f6f298c8bc4a
Kris <109919530+maleker86@users.noreply.github.com>
2024-03-12 19:19:38 -0700

    Add objects which share a function

--- exercises/3-objects.js
+++ exercises/3-objects.js
@@ -1,1 +1,24 @@
-console.log("test");
+var cat = {
+    legs: 3,
+    name: "Harmony",
+    color: "black",
+    speak: speak,
+    sound: "meow",
+    species: "cat",
+};
+
+var pig = {
+    sound: "oink",
+    name: "Charlie",
+    speak: speak,
+}
+
+var horse = {
+    sound: "neigh",
+    name: "Mary",
+    speak: speak,
+}
+
+function speak() {
+    console.log(this.sound + "! My name is " + this.name + "!")
+};
